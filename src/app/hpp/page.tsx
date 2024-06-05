9b02c15afc5b41f431baf7ad88bd751f5401c414
nooglersoon <nooglersoon@outlook.co.id>
2024-06-05 08:06:26 +0700

    add calculation

--- src/app/hpp/page.tsx
+++ src/app/hpp/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 
 interface FormValues {
+  nilaiTukarUSD: number;
   hargaSapiCIF: number;
   beratSapi: number;
   beaMasuk: number;
@@ -18,24 +19,25 @@ interface FormValues {
 
 export default function PriceCalculator() {
   const [values, setValues] = useState<FormValues>({
-    hargaSapiCIF: 0,
-    beratSapi: 0,
+    nilaiTukarUSD: 16285.25,
+    hargaSapiCIF: 2.3,
+    beratSapi: 330,
     beaMasuk: 0,
-    pph: 0,
-    biayaImport: 0,
-    lamaPemeliharaan: 0,
-    biayaPemeliharaan: 0,
-    pertambahanBerat: 0,
-    tingkatBungaBank: 0,
-    biayaOverHead: 0,
-    biayaMarketing: 0,
-    hargaJualSapi: 0,
+    pph: 2.5,
+    biayaImport: 415000,
+    lamaPemeliharaan: 100,
+    biayaPemeliharaan: 55050,
+    pertambahanBerat: 1.6,
+    tingkatBungaBank: 9,
+    biayaOverHead: 10000,
+    biayaMarketing: 175000,
+    hargaJualSapi: 47500,
   });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     // Parse the value to a number, ignoring leading and trailing spaces
-    const parsedValue: number = parseFloat(value.trim());
+    let parsedValue: number = parseFloat(value.trim());
 
     // Check if the parsed value is a valid number and not NaN
     if (!isNaN(parsedValue)) {
@@ -56,12 +58,55 @@ export default function PriceCalculator() {
     e.preventDefault();
     // Here, you can submit the form data to your backend or handle it as needed
     console.log('Form data submitted:', { values });
+    console.log('Nilai Beli Sapi CIF ', calculateNilaiBeliSapiCIF())
+    console.log('Bea Masuk ', calculateBeaMasuk())
+    console.log('PPH Impor ', calculatePPHImpor())
+    console.log('Jumlah ', calculateJumlah())
+    console.log('Harga sapi di kandang ', calculateHargaSapiDiKandang())
+    console.log('Harga pemeliharaan ', calculateHargaPemeliharaan())
+    console.log('Biaya overhead ', calculateBiayaOverhead())
+    console.log('Total Bunga ', calculateBiayaBunga())
+    console.log('Total Biaya ', calculateTotalBiaya())
   };
 
+  const calculateNilaiBeliSapiCIF = (): number => {
+    return values.hargaSapiCIF * values.beratSapi * values.nilaiTukarUSD
+  }
+
+  const calculateBeaMasuk = (): number => {
+    return calculateNilaiBeliSapiCIF() * (values.beaMasuk / 100)
+  }
+
+  const calculatePPHImpor = (): number => {
+    return (calculateNilaiBeliSapiCIF() + calculateBeaMasuk()) * (values.pph / 100)
+  }
+
+  const calculateJumlah = (): number => {
+    return calculateNilaiBeliSapiCIF() + calculateBeaMasuk() + calculatePPHImpor() + values.biayaImport
+  }
+
+  const calculateHargaSapiDiKandang = (): number => {
+    if (!values.beratSapi) { return 0 }
+    return calculateJumlah() / values.beratSapi
+  }
+
+  const calculateHargaPemeliharaan = (): number => {
+    return values.lamaPemeliharaan * values.biayaPemeliharaan
+  }
+
+  const calculateBiayaOverhead = (): number => {
+    return values.lamaPemeliharaan * values.biayaOverHead
+  }
+
+  const calculateBiayaBunga = (): number => {
+    return (calculateJumlah() + calculateHargaSapiDiKandang() + calculateHargaPemeliharaan() + calculateBiayaOverhead() + values.biayaMarketing) * ((values.lamaPemeliharaan / 365) * values.tingkatBungaBank / 100)
+  }
+
+  const calculateTotalBiaya = (): number => {
+    return calculateJumlah() + calculateHargaSapiDiKandang() + calculateHargaPemeliharaan() + calculateBiayaOverhead() + calculateBiayaBunga()
+  }
+
   useEffect(() => {
-    const CC = require('currency-converter-lt')
-    let currencyConverter = new CC({ from: "USD", to: "JPY", amount: 100 })
-    console.log(currencyConverter)
   }, [])
 
   return (
@@ -70,8 +115,12 @@ export default function PriceCalculator() {
         <h2 className="text-2xl font-semibold mb-4">Aplikasi Proyeksi Harga Pokok</h2>
         <div className="flex flex-wrap mb-4">
           <div className="w-full px-2 mb-4 md:mb-0 my-4">
+            <label htmlFor="nilaiTukarUSD" className="block text-gray-700 font-bold">Nilai Tukar USD (Rp)</label>
+            <input type="number" name="nilaiTukarUSD" id="nilaiTukarUSD" value={values.nilaiTukarUSD} onChange={handleChange} className="form-input mt-1 block w-full border border-1 rounded-md px-4 py-2 border-black" />
+          </div>
+          <div className="w-full px-2 mb-4 md:mb-0 my-4">
             <label htmlFor="hargaSapiCIF" className="block text-gray-700 font-bold">Harga Sapi (CIF)</label>
-            <input type="number" name="hargaSapiCIF" id="hargaSapiCIF" value={values.hargaSapiCIF} placeholder={"ahaha"} onChange={handleChange} className="form-input mt-1 block w-full border border-1 rounded-md px-4 py-2 border-black" />
+            <input type="number" name="hargaSapiCIF" id="hargaSapiCIF" value={values.hargaSapiCIF} onChange={handleChange} className="form-input mt-1 block w-full border border-1 rounded-md px-4 py-2 border-black" />
           </div>
           <div className="w-full px-2 mb-4 md:mb-0 my-4">
             <label htmlFor="beratSapi" className="block text-gray-700 font-bold">Berat Sapi (Kg.)</label>
